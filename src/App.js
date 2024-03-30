4f996c8ebfbb6338d1acf0f8804d60ae8ec64cc5
Akash <akashmurali24@gmail.com>
2024-03-30 20:54:21 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [userName, setUsername] = useState("");
+  const [id, setId] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((resopones) => resopones.json())
+      .then((json) => setUsers(json));
+  }, []);
+  console.log(users);
+  const handleDelete = () => {};
   return (
     <div className="App">
       <table className="bp4-html-tabel modifier">
@@ -12,17 +26,36 @@ function App() {
           <th>Action</th>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <th>Akash</th>
-            <th>akashmurali24@gmail.com</th>
-            <th>WWW.akash.com</th>
-            <th>Edit Delete</th>
-          </tr>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td
+                contentEditable={true}
+                value={id}
+                onChange={(e) => setId(e.target.value)}
+                placeholder="edit the id"
+              >
+                {user.id}
+              </td>
+              <td
+                contentEditable={true}
+                value={userName}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="edit name"
+              >
+                {user.name}
+              </td>
+              <td>{user.email}</td>
+              <td>{user.website}</td>
+              <button className="bg-warning px-3">Update</button>
+              <button className="bg-danger m-2" onClick={handleDelete}>
+                Delete
+              </button>
+            </tr>
+          ))}
+          <tr></tr>
         </tbody>
       </table>
     </div>
   );
 }
-
 export default App;
